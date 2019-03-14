b19980d028dfcfa8fe1e4cdfaba1f00f9522749f
Ben Oats <ben.oats@wickes.co.uk>
2019-03-14 12:05:41 +0000

    fixe bootstrap

--- wp-content/themes/twentyseventeen/filmlist.php
+++ wp-content/themes/twentyseventeen/filmlist.php
@@ -6,9 +6,66 @@ get_header();
 ?>
 
     <div class="container">
-    <div id="primary" class="row">
-    <main id="main" class="site-main" role="main">
-        <div class="col-8 padding-set-title">
+        <div class="row">
+            <div class="col-sm">
+
+            </div>
+            <div class="col-8">
+                <h5>Place the films in your preferd order by draging or clicking the up and down arrow. Once you
+                    have made your selection enter tour name and email and email the click "submit"</h5>
+            </div>
+            <div class="col-sm">
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm">
+
+            </div>
+            <div class="col-2">
+                <ol>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                    <li>6</li>
+                    <li>7</li>
+                </ol>
+            </div>
+            <div class="col-6">
+                <ul id="new_fields">
+                    <li id="field_id_1" name='fruits[]' class="ui-state-default">The Abyss</li>
+                    <li id="field_id_2" name='fruits[]' class="ui-state-default">Aliens</li>
+                    <li id="field_id_3" name='fruits[]' class="ui-state-default">The Terminator</li>
+                    <li id="field_id_4" name='fruits[]' class="ui-state-default">Terminator 2: Judgement Day
+                    </li>
+                    <li id="field_id_5" class="ui-state-default">Avatar</li>
+                    <li id="field_id_6" class="ui-state-default">Titanic</li>
+                    <li id="field_id_7" class="ui-state-default">Piranha II: The Spawning</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm">
+here
+            </div>
+            <div class="col-8">
+                <form onclick="sendFilms" method="get">
+                    Name: <input type="text" id="name" name="name"><br>
+                    E-mail: <input type="text" id="email" name="email"><br>
+                    <input type="submit">
+                </form>
+            </div>
+            <div class="col-sm">
+here
+            </div>
+        </div>
+    </div>
+
+
             <?php
             while (have_posts()) :
                 the_post();
@@ -18,47 +75,8 @@ get_header();
                 endif;
             endwhile;
             ?>
-            <h5>Place the films in your preferd order by draging or clicking the up and down arrow. Once you
-                have made your selection enter tour name and email and email the click "submit"</h5>
-            <div class="row">
-                <div class="col-10">
-                    <div class="col-2" style="float: left">
-                        <ol>
-                            <li>1</li>
-                            <li>2</li>
-                            <li>3</li>
-                            <li>4</li>
-                            <li>5</li>
-                            <li>6</li>
-                            <li>7</li>
-                        </ol>
-                    </div>
 
-                    <div class="col-10" style="float: right">
-                        <ul id="new_fields">
-                            <li id="field_id_1" name='fruits[]' class="ui-state-default">The Abyss</li>
-                            <li id="field_id_2" name='fruits[]' class="ui-state-default">Aliens</li>
-                            <li id="field_id_3" name='fruits[]' class="ui-state-default">The Terminator</li>
-                            <li id="field_id_4" name='fruits[]' class="ui-state-default">Terminator 2: Judgement Day
-                            </li>
-                            <li id="field_id_5" class="ui-state-default">Avatar</li>
-                            <li id="field_id_6" class="ui-state-default">Titanic</li>
-                            <li id="field_id_7" class="ui-state-default">Piranha II: The Spawning</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-        <div class="col-8">
-            <form onclick="sendFilms" method="get">
-                Name: <input type="text" id="name" name="name"><br>
-                E-mail: <input type="text" id="email" name="email"><br>
-                <input type="submit">
-            </form>
-        </div>
-        </div>
-    </main>
+
 
     <!--            For list see front end cms page-->
     10<?php get_footer(); ?>
--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -446,12 +446,10 @@ add_action( 'wp_head', 'twentyseventeen_colors_css_wrap' );
 function twentyseventeen_scripts() {
 
     wp_enqueue_style( 'filmsort', get_template_directory_uri() . '/assets/css/filmsort.css' );
-    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.css', array(), 20141119 );
-    wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/css/style.css', array(), 20141119 );
+    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/assets/css/bootstrap.css', array(), 20141119 );
     // all scripts
     wp_enqueue_script( 'filmjquery', get_template_directory_uri() . '/assets/js/filmjquery.js', array( 'jquery' ), '3.3.6', true );
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '20120206', true );
-    wp_enqueue_script( 'theme-script', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '20120206', true );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '20120206', true );
     wp_enqueue_script( 'jquery-ui-core' );
     wp_enqueue_script( 'jquery-ui-widget' );
     wp_enqueue_script( 'jquery-ui-mouse' );
